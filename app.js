5563484c2aaff1314b9d6e99b627b47f4185332a
samueljustus <samie.justus@gmail.com>
2023-11-06 10:07:00 +0100

    complete

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const markAllAsRead = document.querySelector('.mark-as_read')
 const container = document.querySelector('.container')
 const notifications = container.querySelectorAll('.notification')
 const notificationsArray = Array.from(notifications)
+const numberOfNotification = document.querySelector('.number')
 
 
 
 const read = () => {
     notificationsArray.forEach((element) => {
         element.classList.add('background-diffrence', 'reset')
+        numberOfNotification.innerText = 0;
     })
 }
 
